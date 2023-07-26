ad97f6928c5b00a74b4033f4fa41f598b9f1b708
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-07-26 17:34:33 -0600

    Creater footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts"></script>
+
+<template>
+  <footer>
+    <div class="wrapper">
+      <p class="footer-text">© 2023 Abdul-Rahime Kadri</p>
+      <div class="footer-created-with">
+        <p>Created with</p>
+        <img src="../assets/languages/vuejs-original.svg" alt="" />
+        <p>&</p>
+        <img src="../assets/languages/css3-original.svg" alt="" />
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style>
+.wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.5rem 0;
+  gap: 0.7rem;
+  font-size: 0.7rem;
+}
+
+.footer-created-with {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.2rem;
+}
+
+.footer-created-with img {
+  width: 1rem;
+  height: 1rem;
+}
+</style>
