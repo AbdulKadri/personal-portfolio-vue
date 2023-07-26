bdb07ec0271ef598f57786d32d7a02add38953b5
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-07-25 21:33:50 -0600

    Remove boilerplate and add image

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,35 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import Header from '@/components/Header.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <p>Abdul-Rahime Kadri</p>
   </main>
 </template>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+
+  max-width: 100vw;
+  min-height: 80vh;
+  margin: 0 2rem;
+  border-radius: 8px;
+
+  background-image: url('../assets/workplace.jpg');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  box-sizing: border-box;
+}
+</style>
