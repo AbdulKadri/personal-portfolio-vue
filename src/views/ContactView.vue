a0a5a42f0b8bd8756d34556b9cee4f4cd0017baa
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-07-25 22:28:48 -0600

    Create projects and contact views

---
+++ src/views/ContactView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="contact">
+    <h1>Contact</h1>
+    <p>Name ____</p>
+    <p>Email ____</p>
+    <p>Message</p>
+
+    <p>Socials</p>
+    <p>Github</p>
+    <p>LinkedIn</p>
+  </div>
+</template>
+
+<style></style>
---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="projects">
+    <h1>Projects</h1>
+    <h2>Recent</h2>
+    <h4>JavaScript/React/TypeScript</h4>
+    <h4>JavaScript/Vue/TypeScript</h4>
+    <h4>FullStack</h4>
+    <h4>React Native</h4>
+    <h4>PHP/Laravel</h4>
+    <h4>Python/Django</h4>
+    <h4>C#/.NET</h4>
+  </div>
+</template>
+
+<style></style>
