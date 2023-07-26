f1baa41caf8cd90073daa03e5f272f75b227ae24
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-07-26 16:46:25 -0600

    Add font awesome icons

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,28 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
 import { MotionPlugin } from '@vueuse/motion'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {
+  faAddressBook,
+  faArrowRight,
+  faDiagramProject,
+  faHouse,
+  faUser
+} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faArrowRight)
+library.add(faHouse)
+library.add(faUser)
+library.add(faDiagramProject)
+library.add(faAddressBook)
 
 const app = createApp(App)
 
 app.use(router)
 app.use(MotionPlugin)
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')
