951dc9ded680d661765f5a42d5b7f9596104e3ca
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-07-26 22:20:42 -0600

    Add socials and projects section

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,20 @@
 <template>
   <section class="section-card">
     <h2 class="section-title">{{ title }}</h2>
-    <p class="section-description">{{ description }}</p>
+    <p v-if="description && description.length" class="section-description">{{ description }}</p>
+    <div v-if="projects && projects.length" class="projects">
+      <div v-for="project in projects" :key="project.title" class="project">
+        <h3>{{ project.title }}</h3>
+        <img :src="project.image" :alt="project.title" />
+      </div>
+    </div>
+    <div v-if="socials && socials.length" class="socials">
+      <div v-for="social in socials" :key="social.link">
+        <a :href="social.link" target="_blank">
+          <font-awesome-icon :icon="social.icon" size="2x" />
+        </a>
+      </div>
+    </div>
     <StyledLink :link="link" :linkText="linkText" />
   </section>
 </template>
@@ -9,14 +22,18 @@
 <script setup lang="ts">
 import StyledLink from './StyledLink.vue'
 
+type Project = {
+  title: string
+  image: string
+}
+
 const props = defineProps({
   title: {
     type: String,
     required: true
   },
   description: {
-    type: String,
-    required: true
+    type: String
   },
   link: {
     type: String,
@@ -25,6 +42,14 @@ const props = defineProps({
   linkText: {
     type: String,
     default: 'Learn More'
+  },
+  projects: {
+    type: Array as () => Project[],
+    default: () => []
+  },
+  socials: {
+    type: Array as () => Array<{ icon: Array<string>; link: string }>,
+    default: () => []
   }
 })
 </script>
@@ -35,7 +60,7 @@ const props = defineProps({
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  gap: 1rem;
+  gap: 0.5rem;
 
   border-radius: 10px;
   background: #e0e0e0;
@@ -46,5 +71,31 @@ const props = defineProps({
 
 .section-description {
   text-align: left;
+}
+
+.projects {
+  display: flex;
+  gap: 1rem;
+}
+
+.project {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.project img {
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+}
+
+.socials {
+  display: flex;
+  gap: 1rem;
+}
+
+.socials a:hover {
+  color: #00ccff;
 }
 </style>
