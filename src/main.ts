e25458d513508592ac387ccb2189b4e7ab8cce31
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-07-28 00:31:39 -0600

    Add toast for successful email

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import App from './App.vue'
 import router from './router'
 
 import { MotionPlugin } from '@vueuse/motion'
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import {
@@ -29,6 +31,11 @@ library.add(faDisplay)
 const app = createApp(App)
 
 app.use(router)
+app.use(Toast, {
+  transition: 'Vue-Toastification__bounce',
+  maxToasts: 20,
+  newestOnTop: true
+})
 app.use(MotionPlugin)
 app.component('font-awesome-icon', FontAwesomeIcon)
 
