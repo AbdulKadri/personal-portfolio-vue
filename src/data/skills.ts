94c7b8c0ddbb665e828fa77deac301f2549b752d
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-07-29 18:54:36 -0600

    Revert source images back to original

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,64 +1,36 @@
-import HtmlLogo from '../assets/languages/html5-original.svg'
-import CssLogo from '../assets/languages/css3-original.svg'
-import JavaScriptLogo from '../assets/languages/javascript-original.svg'
-import TypeScriptLogo from '../assets/languages/typescript-original.svg'
-import ReactLogo from '../assets/languages/react-original.svg'
-import ReduxLogo from '../assets/languages/redux-original.svg'
-import NextLogo from '../assets/languages/nextjs-original.svg'
-import VueLogo from '../assets/languages/vuejs-original.svg'
-import NodeLogo from '../assets/languages/nodejs-original.svg'
-import ExpressLogo from '../assets/languages/express-original.svg'
-import MongoDBLogo from '../assets/languages/mongodb-original.svg'
-import PhpLogo from '../assets/languages/php-plain.svg'
-import LaravelLogo from '../assets/languages/laravel.svg'
-import MySqlLogo from '../assets/languages/mysql-plain.svg'
-import ThreeLogo from '../assets/languages/threejs-original.svg'
-import GsapLogo from '../assets/languages/gsap-original.svg'
-import SassLogo from '../assets/languages/sass-original.svg'
-import TailwindLogo from '../assets/languages/tailwindcss-plain.svg'
-import FigmaLogo from '../assets/languages/figma-original.svg'
-import MaterialLogo from '../assets/languages/materialui-original.svg'
-import WordpressLogo from '../assets/languages/wordpress-plain.svg'
-import PythonLogo from '../assets/languages/python-original.svg'
-import AwsLogo from '../assets/languages/aws-original.svg'
-import PostmanLogo from '../assets/languages/postman-original.svg'
-import JestLogo from '../assets/languages/jest-plain.svg'
-import CypressLogo from '../assets/languages/cypress.svg'
-import GitLogo from '../assets/languages/git-original.svg'
-
 interface Tool {
   logo: string
   name: string
 }
 
 const tools: Tool[] = [
-  { logo: HtmlLogo, name: 'HTML' },
-  { logo: CssLogo, name: 'CSS' },
-  { logo: JavaScriptLogo, name: 'JavaScript' },
-  { logo: TypeScriptLogo, name: 'TypeScript' },
-  { logo: ReactLogo, name: 'React' },
-  { logo: ReduxLogo, name: 'Redux' },
-  { logo: NextLogo, name: 'Next.js' },
-  { logo: VueLogo, name: 'Vue.js' },
-  { logo: NodeLogo, name: 'Node.js' },
-  { logo: ExpressLogo, name: 'Express.js' },
-  { logo: MongoDBLogo, name: 'MongoDB' },
-  { logo: PhpLogo, name: 'PHP' },
-  { logo: LaravelLogo, name: 'Laravel' },
-  { logo: MySqlLogo, name: 'MySQL' },
-  { logo: ThreeLogo, name: 'Three.js' },
-  { logo: GsapLogo, name: 'GSAP' },
-  { logo: SassLogo, name: 'Sass' },
-  { logo: TailwindLogo, name: 'Tailwind CSS' },
-  { logo: FigmaLogo, name: 'Figma' },
-  { logo: MaterialLogo, name: 'Material UI' },
-  { logo: WordpressLogo, name: 'Wordpress' },
-  { logo: PythonLogo, name: 'Python' },
-  { logo: AwsLogo, name: 'AWS' },
-  { logo: PostmanLogo, name: 'Postman' },
-  { logo: JestLogo, name: 'Jest' },
-  { logo: CypressLogo, name: 'Cypress' },
-  { logo: GitLogo, name: 'Git' }
+  { logo: '/src/assets/languages/html5-original.svg', name: 'HTML' },
+  { logo: '/src/assets/languages/css3-original.svg', name: 'CSS' },
+  { logo: '/src/assets/languages/javascript-original.svg', name: 'JavaScript' },
+  { logo: '/src/assets/languages/typescript-original.svg', name: 'TypeScript' },
+  { logo: '/src/assets/languages/react-original.svg', name: 'React' },
+  { logo: '/src/assets/languages/redux-original.svg', name: 'Redux' },
+  { logo: '/src/assets/languages/nextjs-original.svg', name: 'Next.js' },
+  { logo: '/src/assets/languages/vuejs-original.svg', name: 'Vue.js' },
+  { logo: '/src/assets/languages/nodejs-original.svg', name: 'Node.js' },
+  { logo: '/src/assets/languages/express-original.svg', name: 'Express.js' },
+  { logo: '/src/assets/languages/mongodb-original.svg', name: 'MongoDB' },
+  { logo: '/src/assets/languages/php-plain.svg', name: 'PHP' },
+  { logo: '/src/assets/languages/laravel.svg', name: 'Laravel' },
+  { logo: '/src/assets/languages/mysql-plain.svg', name: 'MySQL' },
+  { logo: '/src/assets/languages/threejs-original.svg', name: 'Three.js' },
+  { logo: '/src/assets/languages/gsap-original.svg', name: 'GSAP' },
+  { logo: '/src/assets/languages/sass-original.svg', name: 'Sass' },
+  { logo: '/src/assets/languages/tailwindcss-plain.svg', name: 'Tailwind CSS' },
+  { logo: '/src/assets/languages/figma-original.svg', name: 'Figma' },
+  { logo: '/src/assets/languages/materialui-original.svg', name: 'Material UI' },
+  { logo: '/src/assets/languages/wordpress-plain.svg', name: 'Wordpress' },
+  { logo: '/src/assets/languages/python-original.svg', name: 'Python' },
+  { logo: '/src/assets/languages/aws-original.svg', name: 'AWS' },
+  { logo: '/src/assets/languages/postman-original.svg', name: 'Postman' },
+  { logo: '/src/assets/languages/jest-plain.svg', name: 'Jest' },
+  { logo: '/src/assets/languages/cypress.svg', name: 'Cypress' },
+  { logo: '/src/assets/languages/git-original.svg', name: 'Git' }
 ]
 
 export default tools
